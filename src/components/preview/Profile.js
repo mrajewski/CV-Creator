123530421c61ae202c2272192ba4d5a7a5c131f1
mrajewski <rajewski.ma@gmail.com>
2020-05-18 18:33:30 +0200

    semantic ui modal added

--- src/components/preview/Profile.js
+++ src/components/preview/Profile.js
@@ -1,17 +1,39 @@
-import React from "react";
+import React, {useState} from "react";
 import {connect} from "react-redux"
+import Modal from 'react-modal';
+import ModalForm from "../modals/ProfileModal";
+
 
 const Profile = ({profile}) => {
+    const [modalIsOpen, setIsOpen] = useState(false);
+
+    const openModal = () => {
+        setIsOpen(true);
+    };
+
+
+    const closeModal = () => {
+        setIsOpen(false);
+    };
+
     return (
-        <section className="section profile">
-            <h2 className="header">{profile.name}</h2>
-            <p className="title">{profile.job}</p>
-            <div className="address">
-                <span>{profile.address}</span>
-                <span>{profile.phone}</span>
-                <span>{profile.mail}</span>
-            </div>
-        </section>
+        <>
+            <section onClick={openModal} className="section profile">
+                <h2 className="header">{profile.name}</h2>
+                <p className="title">{profile.job}</p>
+                <div className="address">
+                    <span>{profile.address}</span>
+                    <span>{profile.phone}</span>
+                    <span>{profile.mail}</span>
+                </div>
+            </section>
+            <ModalForm
+                open={modalIsOpen}
+                close={closeModal}
+            >
+            </ModalForm>
+        </>
+
     )
 };
 

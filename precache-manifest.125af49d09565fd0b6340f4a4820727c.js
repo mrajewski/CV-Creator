eb6bf266945ef024af89946ca1daa1d9faa5a8d5
mrajewski <rajewski.ma@gmail.com>
2020-05-26 16:59:34 +0200

    Updates

---
+++ precache-manifest.125af49d09565fd0b6340f4a4820727c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f13f4edf72b0958fa0fb99b1873a85b8",
+    "url": "/CV-Creator/index.html"
+  },
+  {
+    "revision": "f23276fdabfea6a86057",
+    "url": "/CV-Creator/static/css/main.b7481d49.chunk.css"
+  },
+  {
+    "revision": "d136af1cba199228f133",
+    "url": "/CV-Creator/static/js/2.a795dae7.chunk.js"
+  },
+  {
+    "revision": "0ac79d53dda6ffa0c6325c90109848cb",
+    "url": "/CV-Creator/static/js/2.a795dae7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f23276fdabfea6a86057",
+    "url": "/CV-Creator/static/js/main.80c9753f.chunk.js"
+  },
+  {
+    "revision": "0ad2499b37b6af1e85f4",
+    "url": "/CV-Creator/static/js/runtime-main.a583bfde.js"
+  },
+  {
+    "revision": "7f994e6abb5f8da9cf19191968f9dcd8",
+    "url": "/CV-Creator/static/media/._Kollektif-Bold.7f994e6a.ttf"
+  }
+]);
